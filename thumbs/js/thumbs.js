cc5dd11907ec3ecd1da1e1356c7603f0fe6395b5
Mayara Henriques Silva Sá Santos <estagio.mayara@jfpb.jus.br>
2024-06-11 15:02:41 -0300

    botoes-jquery

---
+++ thumbs/js/thumbs.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	var btn_thumb = $('#thumbs');
+	var contador = $('#thumbs .contador span');
+
+	btn_thumb.on('click', onClick);
+
+	function onClick() {
+		if(btn_thumb.hasClass('disabled')){
+			return;
+		}
+
+		var contagem = parseInt(contador.text());
+		contador.text(contagem + 1);
+
+		btn_thumb.addClass('disabled');
+	};
+});
